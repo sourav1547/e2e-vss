3b56c7b8fe31011bb60aff924b9ea62b4846901a
sourav1547 <souravdas1547@gmail.com>
2024-01-13 14:47:56 -0600

    instance change

--- aws/config.py
+++ aws/config.py
@@ -6,7 +6,7 @@ ACSS_PORT = 13021
 BENCH_RESULT_DIR="experiments"
 
 # EC2 Config
-INSTANCE_TYPE = "c5.2xlarge"
+INSTANCE_TYPE = "m4.xlarge"
 IAM_INSTANCE_PROFILE = {
         "Arn": "arn:aws:iam::395431295218:instance-profile/vss",
 }
@@ -15,12 +15,12 @@ OPEN_PORTS = [
 ]
 # Region: (AMI, VPC)
 REGIONS = {
-        "us-east-2": ("ami-0cd3c7f72edd5b06d", "vpc-17ecf57f"),
-        "us-east-1": ("ami-0005e0cfe09cc9050", "vpc-850798ff"),
-        "us-west-1": ("ami-0a5ed7a812aeb495a", "vpc-ca615aad"),
-        "us-west-2": ("ami-0944e91aed79c721c", "vpc-b6c46cce"),
-        "ca-central-1": ("ami-0b7fec1e45e0e5ae5", "vpc-dff79eb7"),
-        "eu-west-1": ("ami-062a49a8152e4c031", "vpc-43d9dc25"),
-        "ap-northeast-1": ("ami-0506f0f56e3a057a4", "vpc-07a28d60"),
-        "ap-southeast-1": ("ami-0120e0e7231daa18b", "vpc-b31c2ed4"),
+        "us-east-2": ("ami-0c2f3d2ee24929520", "vpc-17ecf57f"),
+        "us-east-1": ("ami-0c0b74d29acd0cd97", "vpc-850798ff"),
+        "us-west-1": ("ami-0f30a20675de7eb6e", "vpc-ca615aad"),
+        "us-west-2": ("ami-00112c992a47ba871", "vpc-b6c46cce"),
+        "ca-central-1": ("ami-0b8f000719123a451", "vpc-dff79eb7"),
+        "eu-west-1": ("ami-02a66cf05465c373f", "vpc-43d9dc25"),
+        "ap-northeast-1": ("ami-027a31eff54f1fe4c", "vpc-07a28d60"),
+        "ap-southeast-1": ("ami-01128ea8b62b5930e", "vpc-b31c2ed4"),
 }
